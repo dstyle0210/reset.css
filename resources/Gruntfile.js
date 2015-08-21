2ed5ff00199f2ba56bae48546d9673250ff3d412
dstyle <dstyle0210@gmail.com>
2015-08-21 18:11:16 +0900

    new file commit

--- resources/Gruntfile.js
+++ resources/Gruntfile.js
@@ -1,59 +1,74 @@
 module.exports = function(grunt) {
-	// Project configuration.
-	grunt.initConfig({
-	    pkg: grunt.file.readJSON('package.json'),
-	    less: {
-			dist: {
-				files: {
-					'reset.css':'reset.less'
-				}
-			}
-		},
-		
-
-		watch: {
-			less:{
-				files: ['reset.less'],
-				/*tasks: ['less','autoprefixer']*/
-				tasks: ['less','autoprefixer','cssmin']
-			}
-		},
-		cssmin: {
-			/*combine: {
-			    files: {
-			      'com/css/dist/public_combine.css': ['com/css/*.css']
-			    } 
-			  },*/
-			dist: {
-				expand: true,
-				src: ['reset.css'],
-				dest:"../",
-				ext: '.min.css'
-			}
-		},
-		autoprefixer : {
-			options : {
-				browsers: ['last 5 version','ie 8','ie 9','ie 10']
-			// Task-specific options go here.
-			},
-			dest:{
-				src:'reset.css'
-			}
-			/*no_dest: {
-				src: 'reset.css' // globbing is also possible here
-			}*/
-		}
-	});
-
-
+	
 	/*
 	 * 작업에 필요한 모듈 로드하기 grunt.loadNpmTasks('grunt-ANY-PLUGIN');
 	 */ 
 	for (var key in grunt.file.readJSON("package.json").devDependencies) {
 		if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
-	}grunt
+	}
+	
+	
+	
+	
+	
+	// Project configuration.
+	grunt.initConfig({
+	    pkg: grunt.file.readJSON('package.json'),
+	    less: {
+			dev: {
+				files:[{
+						expand: true,
+						cwd: './',
+						src: ['*.less'],
+						dest: './',
+						ext: '.css'
+					}]
+			}
+		},
+		watch: {
+			less:{
+				files: ['*.less'],
+				tasks: ['dev']
+			}
+		},
+		cssmin: {
+			dist: {
+				expand: true,
+				src: ['*.css'],
+				dest:"../",
+				ext: '.min.css'
+			}
+		},
+		// Autoprefixer - new postCSS
+		postcss: {
+		  options: {
+		    map: false,
+		    processors: [
+		      require('autoprefixer-core')({browsers: ['last 3 version']})
+		    ]
+		  },
+		  dev: {
+		    src: '*.css'
+		  }
+		},
+		// text Replace by CSS
+		csscomb: {
+			options:{
+				config:"zen.json"
+			},
+		    dynamic_mappings: {
+		        expand: true,
+		        cwd: './',
+		        src: ['*.css'],
+		        dest: './',
+		        ext: '.css'
+		    }
+		}
+	});
+
 
 	// Default task(s).
 	// grunt.registerTask('default', ['less', 'cssmin', 'requirejs', 'watch']);
-	grunt.registerTask('default', ['less','autoprefixer','cssmin','watch']);
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('dev', ['newer:less:dev','newer:postcss:dev','newer:csscomb']);
 };
