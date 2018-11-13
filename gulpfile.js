79b23ec9af3817b9896857dc4c29bed4e87359b7
dstyle0210 <dstyle0210@gmail.com>
2018-11-13 13:03:26 +0900

    Task Runner 수정 , 불필요 소스 삭제

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,92 @@ var sass = require('gulp-sass');
 var csso = require('gulp-csso');
 var csscomb = require('gulp-csscomb');
 var replace = require('gulp-replace');
+var runSequence = require('run-sequence');
 var concatStream = false;
 
+
+
+// gulp value setting
+var gval = {}
+gval.scss = {
+    base:"./src/scss",
+    src:"./src/scss/*.scss",
+    dist:"./src/css"
+};
+gval.css = {
+    base:"./src/css",
+    src:"./src/css/*.css",
+    dist:"./dist"
+};
+
+gulp.task("default",function(callback) {
+    runSequence("scss:build", "scss:watch", function () {
+        console.log(getTimeStamp() + "WORKSPACE READY!");
+    });
+});
+
+gulp.task("scss:build",function(){
+    return pipeLineScss(gulp.src([gval.scss.src],{"base":gval.scss.base}) , gval.scss.dist);
+});
+
+gulp.task("scss:watch",function() {
+    return gulp.watch([scss]).on("change",function(file){
+        var name = path.parse(file.path).base;
+        pipeLineScss( gulp.src(file.path,{"base":gval.scss.base}) , gval.scss.dist);
+        console.log(getTimeStamp() + " [scss:watch] "+name+" changed");
+    });
+});
+
+gulp.task("css:concat",function() {
+    return gulp.src([gval.css.src])
+        .pipe(concat("reset.css"))
+        .pipe(csso({restructure: false}))
+        .pipe(replace('@charset "UTF-8";',''))
+        .pipe(replace('@charset "utf-8";',''))
+        .pipe(insert.prepend('@charset "UTF-8";\n'))
+        .pipe(replace(/}/g,'}\n'))
+        .pipe(replace('/*!','\n/*!'))
+        .pipe(replace('{.','{\n\t.'))
+        .pipe(gulp.dest(gval.css.dist));
+});
+
+gulp.task("css:minify",function() {
+    return gulp.src([gval.css.src])
+        .pipe(concat("reset.min.css"))
+        .pipe(replace('@charset "UTF-8";',''))
+        .pipe(replace('@charset "utf-8";',''))
+        .pipe(replace('/*!','/*'))
+        .pipe(csso({restructure: false}))
+        .pipe(insert.prepend('@charset "UTF-8";\n/* Dstyle reset.css , minify by '+getDateStamp()+'*/\n'))
+        .pipe(gulp.dest(gval.css.dist));
+});
+
+function pipeLineScss(gulpFile , dest){
+    return gulpFile.pipe(sass().on('error', sass.logError))
+        .pipe(csscomb("./zen.json"))
+        .pipe(csso())
+        .pipe(replace(/}/g,'}\n'))
+        .pipe(replace('/*!','\n/*!'))
+        .pipe(replace('{.','{\n\t.'))
+        .pipe(replace('"UTF-8";','"UTF-8";\n'))
+        .pipe(gulp.dest(dest));
+};
+
+function getTimeStamp() {
+    var now = new Date();
+    return ""+(now.getHours() + ':' +((now.getMinutes() < 10)? ("0" + now.getMinutes()): (now.getMinutes())) + ':' +((now.getSeconds() < 10)? ("0" + now.getSeconds()): (now.getSeconds())))+"";
+}
+function getDateStamp() {
+    var now = new Date();
+    var month = ((now.getMonth()+1)<10) ? "0"+ (now.getMonth()+1) : (now.getMonth()+1);
+    var date = (now.getDate()<10) ? "0"+ now.getDate() : now.getDate();
+    return ""+now.getFullYear()+"-"+month+"-"+date;
+}
+
+
+
+
+/*
 gulp.task("default",["scss"],function(){
     gulp.watch(["./src/css/*.css"]).on("change",function(){
         if(concatStream){
@@ -82,7 +166,5 @@ gulp.task("dist",function(){
         });
 });
 
-function getTimeStamp() {
-    var now = new Date();
-    return "["+(now.getHours() + ':' +((now.getMinutes() < 10)? ("0" + now.getMinutes()): (now.getMinutes())) + ':' +((now.getSeconds() < 10)? ("0" + now.getSeconds()): (now.getSeconds())))+"]";
-}
+
+*/
